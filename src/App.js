74bac1ba2a3f1fce6ef5e8225773e076b37e0307
Murat Rakıcı <muratrakici2@gmail.com>
2022-02-22 17:58:57 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { rsi, bollingerBands, bollingerBandsWidth } from 'indicatorts';
+import usdt from "./symbolUsdt.json"
 
-function App() {
+const App = () => {
+  const number = 15
+  const [coin, setcoin] = useState([])
+  useEffect(() => {
+    for (let index = 0; index < usdt.length; index++) {
+      axios.get(`https://api.binance.com/api/v3/klines?symbol=${usdt[index]}&interval=1d`)
+        .then(function (response) {
+          // handle success
+          const closings = response.data.map((i) => Number(i[4]));
+          const resultBol = bollingerBands(closings);
+          const resultRsi = rsi(closings);
+          const resultBolWei = bollingerBandsWidth(resultBol);
+          const resultBolWeiLastElement = resultBolWei.bandWidth.slice(-1)
+          const resultBolWeiAve = resultBolWei.bandWidth.slice(-number).reduce((total, element) => total + element) / number
+
+          if (resultBolWeiLastElement[0] < resultBolWeiAve && resultBolWeiAve < 0.25) {
+            setcoin(s => [...s, usdt[index]])
+            console.log(usdt[index])
+          }
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error);
+        })
+    }
+  }, [])
+
+  const getcoin = (name) => {
+    const coinname = name;
+    let myArray = coinname.split("USDT");
+    const findEmpty = myArray.findIndex((i) => i === "")
+    myArray[findEmpty] = "USDT"
+    console.log(myArray)
+    window.open(`https://www.binance.com/tr/trade/${myArray[0]}_${myArray[1]}`, "_blank");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className='grid'>
+        {coin.map((i, id) => (
+          <div onClick={() => getcoin(i)} className='coin' key={id}>{i}</div>
+        ))}
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/Getsymbol.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Getsymbol = () => {
+    // Güncel Sembol Çekme
+  //   axios.get('https://api.binance.com/api/v3/exchangeInfo')
+  //   .then(function (response) {
+  //     // handle success
+  //     const sembol = response.data.symbols;
+  //     const semboller = sembol.map((i)=>i.symbol)
+  //     console.log(semboller)
+  //     const filteredUser = semboller.filter((data) => {
+  //       return data.toLowerCase().indexOf("usdt".toLowerCase()) !== -1});
+  //     console.log(filteredUser)
+  return (
+    <div>Getsymbol</div>
+  )
+}
+
+export default Getsymbol
